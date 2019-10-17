2fbc0ad744a4d666cca7f1e5b6a9245cfa3c7afe
Pavel Pozdeev <pasharik@mail.ru>
2019-10-17 17:56:57 +0100

    Regex

---
+++ src/week2/playground1/Playground1.kt
@@ -0,0 +1,19 @@
+package week2.playground1
+
+// Implement the function that checks whether a string is a valid identifier.
+// A valid identifier is a non-empty string that starts with a letter or underscore
+// and consists of only letters, digits and underscores.
+
+fun isValidIdentifier(s: String): Boolean {
+    val regex = """^[_a-zA-Z][_a-zA-Z0-9]*""".toRegex()
+    return regex.matches(s)
+}
+
+fun main(args: Array<String>) {
+    println(isValidIdentifier("name"))   // true
+    println(isValidIdentifier("_name"))  // true
+    println(isValidIdentifier("_12"))    // true
+    println(isValidIdentifier(""))       // false
+    println(isValidIdentifier("012"))    // false
+    println(isValidIdentifier("no$"))    // false
+}
