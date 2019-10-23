07b5b7ae618b1a966df616912b5d3ef4836bfb81
Pavel Pozdeev <pasharik@mail.ru>
2019-10-23 20:41:11 +0100

    Mastermind - functional

--- src/week2/example_11_playground_mastermind_fun/PlaygroundMastermindFun.kt
+++ src/week2/example_11_playground_mastermind_fun/PlaygroundMastermindFun.kt
@@ -4,11 +4,11 @@ data class Evaluation(val rightPosition: Int, val wrongPosition: Int)
 
 fun evaluateGuess(secret: String, guess: String): Evaluation {
 
-    val rightPositions = secret.zip(guess).count { (a, b) -> a == b }
+    val rightPositions = secret.zip(guess).count { it.first == it.second }
 
     val commonLetters = "ABCDEF".sumBy { ch ->
 
-        Math.min(secret.count { chS -> chS == ch }, guess.count { chG -> chG == ch })
+        Math.min(secret.count { it == ch }, guess.count { it == ch })
     }
     return Evaluation(rightPositions, commonLetters - rightPositions)
 }
