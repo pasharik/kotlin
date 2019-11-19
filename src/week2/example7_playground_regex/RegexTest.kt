737e6766867c31699195656c648ca080e9924207
Pavel Pozdeev <pasharik@mail.ru>
2019-11-19 22:38:59 +0000

    typo

--- src/week2/example7_playground_regex/RegexTest.kt
+++ src/week2/example7_playground_regex/RegexTest.kt
@@ -1,7 +1,7 @@
 package week2.example7_playground_regex
 
 fun regexTest(s: String) {
-    //With triple quotes don't need to espace back slashes
+    //With triple quotes don't need to escape back slashes
     //toRegex() is an extension function
     val reg = """\d{2}\.\d{2}\.\d{4}""".toRegex()
     reg.matches("12.10.2019") //true
