18d860718ce92b8ccfeb80a2527011449a1eb4fd
Pavel Pozdeev <pasharik@mail.ru>
2020-04-16 14:59:07 +0100

    Lateinit example

---
+++ src/week4/example3_lateinit_exception/LateinitException.kt
@@ -0,0 +1,15 @@
+package week4.example3_lateinit_exception
+
+
+class A {
+    lateinit var foo: String //Should be vaR
+    fun setUp() {
+        foo = "abc"
+    }
+}
+
+fun main() {
+    val a = A()
+    a.setUp() //Need to be called. Otherwise UninitializedPropertyAccessException
+    println(a.foo)
+}
